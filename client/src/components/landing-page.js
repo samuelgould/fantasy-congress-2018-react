dee0192777a5f5e24c69d2bc5524f0f45c754aaf
Sam Gould <sjg326@gmail.com>
2018-04-18 19:23:39 -0700

    built out demo log in

--- client/src/components/landing-page.js
+++ client/src/components/landing-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
+import { login } from '../actions/auth';
 import LoginForm from './login-form';
 import './landing-page.css';
 
@@ -9,14 +10,21 @@ export function LandingPage(props) {
         return <Redirect to="/dashboard" />;
     }
 
+    const demoLogIn = () => {
+        return props.dispatch(login('Demo', 'demopassword'));
+    }
+
     return (
         <div className="landing-page-container">
             <LoginForm />
             
-            <p className="registration-link-text">
-                Don't have an account? <Link to="/register">Register today</Link>.
+            <p className="log-in-page-link-block">
+                Don't have an account? <Link to="/register">Register today.</Link>
             </p>
-            <h3>
+            <p className='log-in-page-link-block'>
+                Or play with Fantasy Congress 2018 using our <Link to='/dashboard' onClick={() => demoLogIn()}><span className='log-in-links-text'>Demo Account.</span></Link>
+            </p>
+            <h3 className='first-time-link-block'>
                 First time? Here's more information about <Link to='/about'>Fantasty Congress 2018</Link>.
             </h3>
         </div>
